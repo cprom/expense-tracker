1ec87c4243257aeed0e920bb40340049293018c4
Chen Prom <cprom@Chens-MBP.lan>
2024-02-02 12:32:04 -0600

    add transaction component working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
 import Header from './components/Header.vue'
 import Balance from './components/Balance.vue'
-import IncomeExpenses from './components/IncomeExpenses.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import AddTransaction from './components/AddTransaction.vue'
 import {ref, computed} from 'vue'
 
-const transactions = ref([
-  {id: 1, text:'Paycheck', amount: 699.99},
-  {id: 2, text:'Food', amount: -20},
-  {id: 3, text:'Bills', amount: -200},
-  {id: 4, text:'Video Game', amount: -54.11},
-  {id: 4, text:'Tax Return', amount: 3000},
+const transactions = ref([])
+// const transactions = ref([
+//   {id: 1, text:'Paycheck', amount: 699.99},
+//   {id: 2, text:'Food', amount: -20},
+//   {id: 3, text:'Bills', amount: -200},
+//   {id: 4, text:'Video Game', amount: -54.11},
+//   {id: 4, text:'Tax Return', amount: 3000},
 
-])
+// ])
+
 
 //get the total
 const total = computed(() => {
@@ -34,6 +37,14 @@ const expense = computed(() => {
   }, 0)
 })
 
+//handle transaction submitted
+const handleTransactionSumbitted = (transactionData) => {
+  transactions.value.push({
+    text: transactionData.text,
+    amount: transactionData.amount,
+
+  })
+} 
 
 </script>
 
@@ -42,5 +53,7 @@ const expense = computed(() => {
 <div class="container">
   <Balance :total="total"></Balance>
   <IncomeExpenses :income="income" :expense="expense"></IncomeExpenses>
+  <AddTransaction @transactionSubmitted="handleTransactionSumbitted"></AddTransaction>
+  <!-- {{ transactions }} -->
 </div>
 </template>
